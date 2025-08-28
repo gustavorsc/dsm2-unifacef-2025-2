5c259276038ec418742cbbcd21215e474c61f605
gustavorsc <127156977+gustavorsc@users.noreply.github.com>
2025-08-28 23:32:11 +0000

    Alteracao da fonte do projeto

--- Aula03/App.js
+++ Aula03/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Configuração do Ambiente Aula3!</Text>
+      <Text style={styles.text}>Configuração do Ambiente Aula3!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +16,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  text: {
+    fontFamily: 'Poppins',
+    fontSize: 20
+  }
+  
 });
+
+
