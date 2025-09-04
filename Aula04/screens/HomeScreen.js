df83a957156c0ddee1e8eea3d2ff1eba31fd1cb9
gustavorsc <127156977+gustavorsc@users.noreply.github.com>
2025-09-04 22:51:55 +0000

    Adcionar atividade 4

---
+++ Aula04/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import {View, Text, button, StyleSheet, Button} from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Tela Inicial</Text>
+            <Button
+                title="ir para detalhes"
+                onPress={() => navigation.navigate('Detalhes')}
+            />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+    titulo: { fontSize: 24, fontWeight: 'bold'}
+});
